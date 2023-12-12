68b4a81b813ca6c95d7055a7b265b25b2d9523c4
pretzelogical <yingyangyo266@gmail.com>
2023-12-12 15:59:57 -0600

    Add  works from json to firestore emu

--- fstore_upload/fstore-upload.js
+++ fstore_upload/fstore-upload.js
@@ -1,4 +1,5 @@
 const admin = require('firebase-admin');
+const fs = require('fs');
 
 // Initialize the app to point to the Firestore emulator
 // Assuming the Firestore emulator is running on the default port
@@ -9,11 +10,39 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
+fs.readFile('./fstore.json', 'utf8', (error, rawData) => {
+    if (error) {
+        console.error('Error reading file:', error);
+        return;
+    }
+    try {
+        const jsonData = JSON.parse(rawData);
+		let worksAdded = 0;
+        console.log(jsonData.works);
+		if (jsonData.works instanceof Array) {
+			jsonData.works.forEach(work => {
+				db.collection('works').doc(`work-${++worksAdded}`).set(work);
+			});
+		}
+    } catch (parseError) {
+        console.error('Error parsing JSON:', parseError);
+    }
+});
+
+// Get all items from works
+db.collection('works')
+	.get()
+	.then(snapshot => {
+		snapshot.forEach(doc => {
+			console.log(doc.id, '=>', doc.data());
+		});
+	})
+
 // Now you can interact with the Firestore emulator
 // Example: Adding a document to a collection
-const docRef = db.collection('users').doc('alovelace');
-docRef.set({
-  first: 'Ada',
-  last: 'Lovelace',
-  born: 1815
-});
+// const docRef = db.collection('users').doc('alovelace');
+// docRef.set({
+//   first: 'Ada',
+//   last: 'Lovelace',
+//   born: 1815
+// });
